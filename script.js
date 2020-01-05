5eda7813e66e8f2b3c35110f738f930e06b22bb5
ZROSENSOHN LAPTOP <zac@satprepct.com>
2020-01-04 22:17:23 -0500

    made solid progress, for the most part working, still needs cleaning up

--- script.js
+++ script.js
@@ -11,8 +11,14 @@
 //     console.log("no local");
 //   }
 
+//set city array to empty and api key
 var cityArr = [];
+var apiKey = "166a433c57516f51dfab1f7edaed8413";
 
+//start by getting values from storage if they exist
+getStorage();
+
+//append li elements of cities from the city array
 function generateCityList(){
 
     $("#cities").empty();
@@ -23,30 +29,110 @@ function generateCityList(){
 
 };
 
+//stringify cities array and send to local storage
+function pushToStorage() {    
+    let jsonData = JSON.stringify(cityArr);
+    localStorage.setItem("savedCities", jsonData);
+};
+
+//get saved array from storage and generate list of saved cities
+function getStorage() {
+    let storedData = localStorage.getItem("savedCities");
+    console.log(storedData);
+    if (storedData !== null){
+        cityArr = JSON.parse(storedData);
+        generateCityList();
+    }
+};
+
 function runAJAX(city) {
-    let apiKey = "166a433c57516f51dfab1f7edaed8413";
     $.ajax({
       url: "http://api.openweathermap.org/data/2.5/forecast?units=imperial&q=" + city + ",us&APPID=" + apiKey,
-      method: "GET"
-    }).then(function(response){
-      console.log(response);
+      method: "GET",
+      success: function(response){
+        
+        if ($.inArray(city, cityArr) === -1) {
+            //add new city to array and push to storage
+            cityArr.push(city);
+            pushToStorage();
+            //Generate up-to-date city list
+            generateCityList();
+        }
+
+        //create all of the weather widgets
+        currentWeater(city);
+        createFiveDay(response);
+      },
+      error: function(xhr) {
+          alert("Error " + xhr.status + ": Not a valid City");
+      }
     })
-  };
+};
 
+function currentWeater(city) {
+    
+    $.ajax({
+        url: "http://api.openweathermap.org/data/2.5/weather?units=imperial&q=" + city + ",us&APPID=" + apiKey,
+        method: "GET"
+    }).then((response) => {
+        let currentDate = moment().format("dddd[, ] MMMM Do[, ] YYYY");
+        let iconcode = response.weather[0].icon;
+        let iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+    
+        $("#currentWeaterCard").attr("class", "card");
+        $("#selectedCity").text(response.name + " (" + currentDate + ")" );
+        $("#wicon").attr("src", iconurl);
+        $("#wicon").css("display", "block");
+        $("#currentDescription").text(response.weather[0].description);
+        $("#currentTemp").text("Temperature: " + response.main.temp + " (F)");
+        $("#currentHumidity").text("Humidity: " + response.main.humidity + "%");
+        $("#currentWind").text("Wind Speed: " + response.wind.speed + " MPH");
+        $("#fiveDayH4").text("Five Day Forecast");
+    })
+}
 
+function createFiveDay(response) {
+    console.log(response);
+
+    $("#fiveDay").empty();
+
+    for(i=0 ; i<response.list.length; i+=8) {
+        console.log(i);
+        let iconcode = response.list[i].weather[0].icon;
+        let iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+        let day = moment(response.list[i].dt_txt);
+        
+        let containerEl = $("<div>").attr("class", "futureDay card");
+        let cardContainerEL = $("<div>").attr("class", "card-body");
+        let iconEl = $("<img>").attr({"src": iconurl, "alt": "weather image"});
+        let dowDiv = $("<div>").text(day.format("dddd")).attr({"class": "font-weight-bold"});
+        let dateDiv = $("<div>").text(day.format("L"));
+        let tempDiv = $("<div>").text("Temp " + response.list[i].main.temp + " (F)");
+        let humidityDiv = $("<div>").text("Humidity: " + response.list[i].main.humidity + "%");
+
+        cardContainerEL.append(dowDiv, dateDiv, iconEl, tempDiv, humidityDiv);
+        containerEl.append(cardContainerEL);
+        $("#fiveDay").append(containerEl);
+    }
+};
+
+//event listener on serach button
+//Search and pull up new city make api call, push to storage, generate updated list of cities
 $("#newCity").on("click", function(e){
     e.preventDefault();
-    
+    //uppercase the added city
     let city = $("#cityInput").val().toUpperCase();
-    cityArr.push(city);
-
     //API Call
     runAJAX(city);
-    
-    // pushToStorage();
-    generateCityList();
-
-    //clear input field
+    //clear input field before next addition
     $("#cityInput").val("");
 
 });
+
+//event listener on list of cities
+//switch views between saved cities
+$("#cities").on("click", "li", function(e){
+    e.preventDefault();
+    let city = $(this).attr("id");
+    runAJAX(city);
+})
